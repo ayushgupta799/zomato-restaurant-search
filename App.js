9d62d6a5cedf98eb7b294c7db22e62ffab034471
Ayush Gupta <guptaayush782@outlook.com>
2021-02-09 13:59:02 +0530

    made results show page

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
 import {createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack'
 // import SearchScreen from "./src/screens/SearchScreen";
